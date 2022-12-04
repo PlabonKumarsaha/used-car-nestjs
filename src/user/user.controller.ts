36835b6d046909935ecb3cad5e13bacfc226d324
pkumarsaha71@gmail.com <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-12-04 23:16:20 +0600

    add : create user api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUser } from './dtos/create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('auth')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Post('/signup')
   createUser(@Body() createUser: CreateUser) {
-    console.log(createUser);
+    this.userService.create(createUser.email, createUser.password);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.userRepo.create({ email, password });
+    return this.userRepo.save(user);
+  }
+}
