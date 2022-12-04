4efa3689384002f4662abfba7da42d473d8b7cdd
pkumarsaha71@gmail.com <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-12-05 00:04:09 +0600

    add :after insert ,update feature

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,11 +1,29 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { IsEmail } from 'class-validator';
+import {
+  AfterInsert,
+  AfterUpdate,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+} from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
   @Column()
+  @IsEmail()
   email: string;
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Your insert id is ' + this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Your update id is ' + this.id);
+  }
 }
