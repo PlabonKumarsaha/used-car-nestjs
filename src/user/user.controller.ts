9a627db9e5b5788de9e69b844cf89f4cd98d33b1
pkumarsaha71@gmail.com <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-12-07 09:19:48 +0600

    add : update and remove user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,15 @@
-import { Body, Controller, Get, Post, Param, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Query,
+  Delete,
+  Patch,
+} from '@nestjs/common';
 import { CreateUser } from './dtos/create-user.dto';
+import { UpdateUser } from './dtos/update-user.dto';
 import { UserService } from './user.service';
 
 @Controller('auth')
@@ -18,5 +28,14 @@ export class UserController {
   @Get()
   findAllUser(@Query('email') email: string) {
     return this.userService.find(email);
+  }
+  @Delete('/:id')
+  deleteUSer(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
+  }
+
+  @Patch()
+  updateUser(@Param('id') id: string, @Body() updateUser: UpdateUser) {
+    return this.userService.update(parseInt(id), updateUser);
   }
 }
